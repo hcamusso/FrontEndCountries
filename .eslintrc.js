6256f0a18dfc65f1cb42d2a037d1de8be8f7763e
hcamusso <hcamusso@gmail.com>
2022-11-29 15:40:30 -0300

    parsing error

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     "overrides": [
     ],
     "parserOptions": {
-        "ecmaVersion": "latest",
+        "ecmaVersion": 12,
         "sourceType": "module"
     },
     "plugins": [
